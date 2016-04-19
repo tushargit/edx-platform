9b48e9d04ba0bca97e282bd6eba401ee2169d3c0
Matt Drayer <mattdrayer@edx.org>
2016-04-18 21:47:43 -0400

    Revert "Revert "saleem-latif/WL-328: Multi-Site Comprehensive Theming""

--- openedx/core/storage.py
+++ openedx/core/storage.py
@@ -2,18 +2,23 @@
 Django storage backends for Open edX.
 """
 from django_pipeline_forgiving.storages import PipelineForgivingStorage
-from django.contrib.staticfiles.storage import StaticFilesStorage, CachedFilesMixin
-from pipeline.storage import PipelineMixin, NonPackagingMixin
+from django.contrib.staticfiles.storage import StaticFilesStorage
+from pipeline.storage import NonPackagingMixin
 from require.storage import OptimizedFilesMixin
-from openedx.core.djangoapps.theming.storage import ComprehensiveThemingAwareMixin
+from openedx.core.djangoapps.theming.storage import (
+    ThemeStorage,
+    ComprehensiveThemingCachedFilesMixin,
+    ThemePipelineMixin
+)
 
 
 class ProductionStorage(
+        # TODO: Investigate the effect of this on theming
         PipelineForgivingStorage,
-        ComprehensiveThemingAwareMixin,
         OptimizedFilesMixin,
-        PipelineMixin,
-        CachedFilesMixin,
+        ThemePipelineMixin,
+        ComprehensiveThemingCachedFilesMixin,
+        ThemeStorage,
         StaticFilesStorage
 ):
     """
@@ -24,9 +29,9 @@ class ProductionStorage(
 
 
 class DevelopmentStorage(
-        ComprehensiveThemingAwareMixin,
         NonPackagingMixin,
-        PipelineMixin,
+        ThemePipelineMixin,
+        ThemeStorage,
         StaticFilesStorage
 ):
     """
